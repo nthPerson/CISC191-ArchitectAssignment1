187bcc8b6923e38ea367593b5864ad10fe0e7a17
Robert Ashe <robertashe2.0@gmail.com>
2023-03-08 18:01:10 -0800

    Added SportUtilityVehicle.java class body

--- Server/src/main/java/edu/sdccd/cisc191/template/SportUtilityVehicle.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/SportUtilityVehicle.java
@@ -1,4 +1,23 @@
 package edu.sdccd.cisc191.template;
 
 public class SportUtilityVehicle extends Vehicle {
+    private int maxTowingWeight;
+    public SportUtilityVehicle(String make, int miles, int price, int numSeats, Option[] options, int maxWeight) {
+        setManufacturerName(make);
+        setMilesOnVehicle(miles);
+        setPrice(price);
+        setNumberOfSeats(numSeats);
+        setOptions(options);
+        setMaxTowingWeight(maxWeight);
+    }
+
+    public int getMaxTowingWeight() {
+        return maxTowingWeight;
+    }
+
+    public void setMaxTowingWeight(int maxWeight) {
+        maxTowingWeight = maxWeight;
+    }
+
 }
+
