cc480f3d2585cb79b5fd81e3e7542ca1c1dd0290
Robert Ashe <robertashe2.0@gmail.com>
2023-03-08 18:43:22 -0800

    Added GasCar.java class body

--- Server/src/main/java/edu/sdccd/cisc191/template/GasCar.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/GasCar.java
@@ -1,4 +1,24 @@
 package edu.sdccd.cisc191.template;
 
 public class GasCar extends Car implements Combustible {
+    private int fuelLevel = 0;
+
+    public GasCar(String make, int miles, int price, int numSeats, Option[] options, int numDoors) {
+        setManufacturerName(make);
+        setMilesOnVehicle(miles);
+        setPrice(price);
+        setNumberOfSeats(numSeats);
+        setOptions(options);
+        setNumberOfDoors(numDoors);
+    }
+
+    @Override
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
+
+    @Override
+    public void tankUp() {
+        fuelLevel = 100;
+    }
 }
