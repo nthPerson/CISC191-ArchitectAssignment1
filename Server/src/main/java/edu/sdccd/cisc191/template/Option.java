f79a88da1e6796e5c4f5a25f863b39e29f7eb387
Robert Ashe <robertashe2.0@gmail.com>
2023-03-08 17:59:26 -0800

    Added Option.java class body

--- Server/src/main/java/edu/sdccd/cisc191/template/Option.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/Option.java
@@ -1,4 +1,14 @@
 package edu.sdccd.cisc191.template;
 
 public class Option {
+    private String details;
+    public Option() {}
+
+    public Option(String opt) {
+        details = opt;
+    }
+
+    public String getDetails() {
+        return details;
+    }
 }
