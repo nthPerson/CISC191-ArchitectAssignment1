9ed5cd8d7602d3b6a3edf6f5ec0a2e20ad9cf594
Robert Ashe <robertashe2.0@gmail.com>
2023-03-08 17:57:11 -0800

    Added Vehicle.java class body

--- Server/src/main/java/edu/sdccd/cisc191/template/Vehicle.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/Vehicle.java
@@ -1,4 +1,59 @@
 package edu.sdccd.cisc191.template;
 
 public class Vehicle {
+    private String manufacturerName;
+    private int milesOnVehicle;
+    private int price;
+    private int numberOfSeats;
+    private Option[] options;
+    public Vehicle() {}
+
+    public Vehicle(String make, int miles, int price, int numSeats, Option[] options) {
+        manufacturerName = make;
+        milesOnVehicle = miles;
+        this.price = price;
+        numberOfSeats = numSeats;
+        this.options = options;
+
+    }
+    public void setManufacturerName(String make) {
+        manufacturerName = make;
+    }
+
+    public void setMilesOnVehicle(int miles) {
+        milesOnVehicle = miles;
+    }
+
+    public void setPrice(int newPrice) {
+        price = newPrice;
+    }
+
+    public void setNumberOfSeats(int numSeats) {
+        numberOfSeats = numSeats;
+    }
+
+    public void setOptions(Option[] options) {
+        this.options = options;
+    }
+
+    public String getManufacturerName() {
+        return manufacturerName;
+    }
+
+    public int getMilesOnVehicle() {
+        return milesOnVehicle;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public Option[] getOptions() {
+        return options;
+    }
 }
+
