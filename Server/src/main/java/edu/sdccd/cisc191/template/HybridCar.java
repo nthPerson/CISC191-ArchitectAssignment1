c9eaf1e15edeceac499c8c421239a1b1e2f230f0
Robert Ashe <robertashe2.0@gmail.com>
2023-03-08 18:46:00 -0800

    Added HybridCar.java class body

--- Server/src/main/java/edu/sdccd/cisc191/template/HybridCar.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/HybridCar.java
@@ -1,4 +1,40 @@
 package edu.sdccd.cisc191.template;
 
 public class HybridCar extends Car implements Combustible, Chargeable {
+    private int batteryCharge;
+    private int fuelLevel = 0;
+
+    public HybridCar(String make, int miles, int price, int numSeats, Option[] options, int numDoors) {
+        setManufacturerName(make);
+        setMilesOnVehicle(miles);
+        setPrice(price);
+        setNumberOfSeats(numSeats);
+        setOptions(options);
+        setNumberOfDoors(numDoors);
+    }
+
+
+    @Override
+    public int getBatteryCharge() {
+        return batteryCharge;
+    }
+
+    public void setBatteryCharge(int batteryCharge) {
+        this.batteryCharge = batteryCharge;
+    }
+
+    @Override
+    public void chargeUp() {
+        batteryCharge = 100;
+    }
+
+    @Override
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
+
+    @Override
+    public void tankUp() {
+        fuelLevel = 100;
+    }
 }
