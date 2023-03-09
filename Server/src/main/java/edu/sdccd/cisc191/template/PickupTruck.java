5f8f4fee83084d3e124e796dcc3f5e5e2611206f
Robert Ashe <robertashe2.0@gmail.com>
2023-03-08 18:00:29 -0800

    Added PickupTruck.java class body

--- Server/src/main/java/edu/sdccd/cisc191/template/PickupTruck.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/PickupTruck.java
@@ -1,4 +1,23 @@
 package edu.sdccd.cisc191.template;
 
 public class PickupTruck extends Vehicle {
+    private int cargoCapacity;
+
+    public PickupTruck(String make, int miles, int price, int numSeats, Option[] options, int cargoCapacity) {
+        setManufacturerName(make);
+        setMilesOnVehicle(miles);
+        setPrice(price);
+        setNumberOfSeats(numSeats);
+        setOptions(options);
+        setCargoCapacity(cargoCapacity);
+
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapcity) {
+        cargoCapacity = cargoCapcity;
+    }
 }
