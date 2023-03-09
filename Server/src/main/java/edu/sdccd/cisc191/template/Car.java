40af950659a4b1eda23b94affd43181bdb5aea82
Robert Ashe <robertashe2.0@gmail.com>
2023-03-08 17:59:46 -0800

    Added Car.java class body

--- Server/src/main/java/edu/sdccd/cisc191/template/Car.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/Car.java
@@ -1,4 +1,25 @@
 package edu.sdccd.cisc191.template;
 
 public class Car extends Vehicle {
+    private int numberOfDoors;
+
+    public Car(String make, int miles, int price, int numSeats, Option[] options, int numDoors) {
+        setManufacturerName(make);
+        setMilesOnVehicle(miles);
+        setPrice(price);
+        setNumberOfSeats(numSeats);
+        setOptions(options);
+        setNumberOfDoors(numDoors);
+    }
+
+    public Car() {
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numDoors) {
+        numberOfDoors = numDoors;
+    }
 }
