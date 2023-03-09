b611d6f59328819cd10504b4c7d3ca6d8e0f7d62
Robert Ashe <robertashe2.0@gmail.com>
2023-03-08 18:46:20 -0800

    Added ElectricCar.java class body

--- Server/src/main/java/edu/sdccd/cisc191/template/ElectricCar.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/ElectricCar.java
@@ -1,4 +1,27 @@
 package edu.sdccd.cisc191.template;
 
 public class ElectricCar extends Car implements Chargeable {
+    private int batteryCharge;
+    public ElectricCar(String make, int miles, int price, int numSeats, Option[] options, int numDoors) {
+        setManufacturerName(make);
+        setMilesOnVehicle(miles);
+        setPrice(price);
+        setNumberOfSeats(numSeats);
+        setOptions(options);
+        setNumberOfDoors(numDoors);
+    }
+
+    public void setBatteryCharge(int charge) {
+        batteryCharge = charge;
+    }
+
+    @Override
+    public int getBatteryCharge() {
+        return batteryCharge;
+    }
+
+    @Override
+    public void chargeUp() {
+        batteryCharge = 100;
+    }
 }
